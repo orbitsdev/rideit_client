08bd873cef3794fa2c95cc3a3ff2335773fe0b31
brian <orbinobrian0506@gmail.com>
2022-03-22 18:43:59 +0800

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -40,7 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    paymentmethod = "none";
+    paymentmethod = "null";
   }
 
   @override
